4e3bb95e138b6125c9268720af807f65214f2871
lulunev <lunev@MacBook-Pro-Daniel.local>
2015-04-22 21:49:27 +0300

    Simple Playlist Browser

---
+++ Algorhythm/PlaylistDetailViewController.swift
@@ -0,0 +1,36 @@
+import UIKit
+
+class PlaylistDetailViewController: UIViewController {
+
+    var playlist: Playlist?
+    @IBOutlet weak var playlistCoverImage: UIImageView!
+    @IBOutlet weak var playlistTitle: UILabel!
+    @IBOutlet weak var playlistDescription: UILabel!
+    @IBOutlet weak var playlistArtist0: UILabel!
+    @IBOutlet weak var playlistArtist1: UILabel!
+    @IBOutlet weak var playlistArtist2: UILabel!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if playlist != nil {
+            playlistCoverImage.image = playlist!.largeIcon
+            playlistCoverImage.backgroundColor = playlist!.backgroundColor
+            playlistTitle.text = playlist!.title
+            playlistDescription.text = playlist!.description
+            playlistArtist0.text = playlist!.artists[0]
+            playlistArtist1.text = playlist!.artists[1]
+            playlistArtist0.text = playlist!.artists[2]
+            
+        }
+    
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+
+}
